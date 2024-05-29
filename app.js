ae5f622eb5b47d3c792dc5127d315b548e41a6fa
GamingHazard <bb466882@gmail.com>
2024-05-29 13:05:55 +0300

    made up a few updates

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+require("dotenv").config(); // Require dotenv and configure it
 
-// Require dotenv and configure it
-require("dotenv").config();
+const app = express();
 
 // MongoDB connection
 mongoose
@@ -16,15 +16,18 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-mongoose.Promise = global.Promise;
-
-const app = express();
-
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan("dev"));
-app.use(cors()); // Enable CORS
+
+// Enable CORS
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "*", // Replace with your frontend URL
+    optionsSuccessStatus: 200,
+  })
+);
 
 // Routes
 const userRoutes = require("./routes/users");
@@ -36,7 +39,6 @@ const plantationRoutes = require("./routes/plantation");
 const recordRoutes = require("./routes/records");
 const salesRoutes = require("./routes/sales");
 const storageRoutes = require("./routes/storage");
-// const postRoutes = require("./routes/posts");
 const shopRoutes = require("./routes/shop");
 const toolRoutes = require("./routes/tools");
 const lossRoutes = require("./routes/loss");
@@ -51,7 +53,6 @@ app.use("/plantation", plantationRoutes);
 app.use("/records", recordRoutes);
 app.use("/sales", salesRoutes);
 app.use("/storage", storageRoutes);
-// app.use("/posts", postRoutes);
 app.use("/shop", shopRoutes);
 app.use("/tools", toolRoutes);
 app.use("/loss", lossRoutes);
@@ -63,9 +64,9 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something went wrong!");
 });
 
-const HOST = "0.0.0.0"; // Specify the IP address here "192.168.188.3" ||
-// Start server
-const PORT = process.env.PORT || 10000;
+const HOST = "0.0.0.0"; // Host
+const PORT = process.env.PORT || 10000; // Port
+
 app.listen(PORT, HOST, () => {
-  console.log(`Server is running on  ${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
