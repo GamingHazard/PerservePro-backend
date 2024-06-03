cd78b0b70c05e24a103094171e504746a9316328
GamingHazard <bb466882@gmail.com>
2024-06-03 20:57:20 +0300

    final commit

--- routes/marketRoutes.js
+++ routes/marketRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const multer = require("multer");
-const Market = require("./models/Market");
+const Market = require("../models/Market");
 const router = express.Router();
 const path = require("path");
 
